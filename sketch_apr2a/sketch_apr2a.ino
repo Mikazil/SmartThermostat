66c3ba8f62de3d155db4b5d01633d9881fc7b32c
Mika <akimivanov43@gmail.com>
2023-04-10 21:15:55 +0700

    SOIL MOISTURE!!!

--- sketch_apr2a/sketch_apr2a.ino
+++ sketch_apr2a/sketch_apr2a.ino
@@ -3,30 +3,14 @@
 #include <Wire.h>
 #include <DHT.h>
 #include <FastBot.h>
-#include "EspMQTTClient.h"
-
-unsigned long timing;
-EspMQTTClient client(
-  "Official WIFI",
-  "25102009",
-  "mqtt.dealgate.ru",  // MQTT Broker server ip
-  "mikazil",           // Can be omitted if not needed
-  "32740238",          // Can be omitted if not needed
-  "TestClient"         // Client name that uniquely identify your device
-);
-
-//Переменные
 #define DHTPIN 4
 float temperature, humidity;
-
+int grass;
 DHT dht(DHTPIN, DHT11);
 const char* ssid = "Official WIFI";
 const char* password = "25102009";
-
-
 ESP8266WebServer server(80);
 FastBot bot("6014815330:AAFla1qhdPpp87zKIKyhx-KUxLpqHbaQp6E");
-
 
 void setup() {
 
@@ -40,14 +24,7 @@ void setup() {
   server.onNotFound(handle_NotFound);
   server.begin();
   Serial.println("HTTP server started");
-
   bot.attach(newMsg);
-}
-
-void onConnectionEstablished() {
-  client.publish("climat/temp", String(dht.readTemperature()));
-  client.publish("climat/hum", String(dht.readHumidity()));
-
 }
 
 void newMsg(FB_msg& msg) {
@@ -55,6 +32,7 @@ void newMsg(FB_msg& msg) {
   else if (msg.text == "Показать температуру") {
     bot.sendMessage("Температура дома: " + String(temperature) + "°C", msg.chatID);
     bot.sendMessage("Влажность дома: " + String(humidity) + "%", msg.chatID);
+    bot.sendMessage("Влажность почвы: " + String(map(analogRead(0), 1024, 280, 0, 100)) + "%", msg.chatID);
   } else {
     bot.sendMessage("Чумба, я только учусь, а ты такое сложное просишь. Давай ограничимся командой \"Показать температуру\"", msg.chatID);
   }
@@ -65,7 +43,6 @@ void loop() {
   humidity = dht.readHumidity();
   server.handleClient();
   bot.tick();
-  client.loop();
 }
 
 void handle_OnConnect() {
@@ -83,15 +60,19 @@ String SendHTML(float temperature, float humidity) {
   ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr += "<title>Weather Station</title>\n";
   ptr += "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css\" integrity=\"sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e\" crossorigin=\"anonymous\">\n";
+  
   //CSS STYLE
-  ptr += "<style>@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap');\n";
-  ptr += "html { font-family: 'Ubuntu', sans-serif; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "<style>@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');\n";
+  ptr += "html { font-family: 'Roboto', sans-serif; display: inline-block; margin: 0px auto;}\n";
   ptr += "body{height: 100vh; background-color: #dcebf3;} h1 {color: #444444;margin: 50px auto 30px; font-weight: 400;}\n";
+  ptr += "h1{text-align: left;  font-size: 42px;  padding-left: 20px;     padding-bottom: 10%;}\n";
   ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px; font-weight: 300;}\n";
-  ptr += ".container {  display: flex;  flex-direction: row;  justify-content: space-between;  align-items: stretch;  height: 200px;}\n";
+  ptr += ".container {  display: flex;  flex-direction: row;  justify-content: space-between;  align-items: stretch;  height: 250px;  text-align: center;}\n";
   ptr += "div.temp{ flex: 0 0 47%;  background-color: #ffffff;  width: 25vh; height: 25vh; position: relative; border-radius: 35px;}\n";
   ptr += "div.hum{  flex: 0 0 47%;  background-color: #ffffff;  width: 25vh; height: 25vh;  position: relative; border-radius: 35px;}\n";
+  ptr += "div.grass{height: 50px;  background: white;  border-radius: 13px;  display: flex;}\n";  
   ptr += "</style>\n";
+  
   //ПРОДОЛЖЕНИЕ ДОКУМЕНТА
   ptr += "</head>\n";
   ptr += "<body>\n";
@@ -117,8 +98,18 @@ String SendHTML(float temperature, float humidity) {
   ptr += "%</p>";
   ptr += "<a style=\"font-size: 12px;\"> Humidity </a>\n";
   ptr += "</div>\n";
-
   ptr += "</div>\n";
+  
+//Влажность почвы
+  ptr +="<div class=\"grass\">\n";
+  ptr +="<a class=\"name\" style=\"font-size: 20px; text-align: left; padding: 3.5%;\"> soil moisture <span style=\"  color: #a3a3a3; font-size: 15px;  font-weight: 700;\">/%</span></a>\n";
+  ptr +="<a style=\"position: inherit;  text-align: right;  font-size: 35px;  padding: 1.5%;  margin-left: auto;  margin-right: 2%;\">";
+  ptr += String(map(analogRead(0), 1024, 280, 0, 100));
+  ptr += "</a>\n";
+  ptr += "</div>\n";
+
+
+//конец документа
   ptr += "</body>\n";
   ptr += "</html>\n";
   return ptr;
@@ -127,9 +118,7 @@ String SendHTML(float temperature, float humidity) {
 void setupwlan() {
   Serial.println("Connecting to ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
